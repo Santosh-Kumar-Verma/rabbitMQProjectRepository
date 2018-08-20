a7b6ff659c3ca8c88d4307073cf8f893ceb71802
Santosh Kumar verma <verma005santosh@gmail.com>
2018-08-20 10:48:27 +0530

    commit by santosh

---
+++ src/main/java/com/rabbitmq/demo/rabbitmqSPDemo/RabbitmqSpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.demo.rabbitmqSPDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqSpDemoApplication.class, args);
+		System.out.println("application started propery");
+	}
+}
